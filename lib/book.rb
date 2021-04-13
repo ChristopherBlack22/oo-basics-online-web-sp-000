e473899c981dd0079b2de559764ed6d01e247515
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-13 16:58:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,11 +3,9 @@ class Book
     @title = title 
   end 
     
-  def title
-    @title
-  end 
+  attr_reader :title
   
-  attr_accessor:author :page_count :genre 
+  attr_accessor:author, :page_count, :genre 
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
